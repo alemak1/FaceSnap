7b0c8183ef475ba82ce68d2f3864d6374eb2ddba
yongrenwang <czie.alexmak@gmail.com>
2017-01-26 18:03:52 +0800

    Added files

---
+++ FaceSnap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FaceSnap
+//
+//  Created by Aleksander Makedonski on 1/26/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import UIKit
+
+class PhotoListController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
