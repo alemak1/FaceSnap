5eabc050468b235c86bc3a810a62bc2f32692fc9
yongrenwang <czie.alexmak@gmail.com>
2017-01-26 18:19:21 +0800

    Added new file MediaPickerManager with corresponding class that subclasses NSObject so that it acts as an Objective-C object

---
+++ FaceSnap/MediaPickerManager.swift
@@ -0,0 +1,13 @@
+//
+//  MediaPickerManager.swift
+//  FaceSnap
+//
+//  Created by Aleksander Makedonski on 1/26/17.
+//  Copyright © 2017 Changzhou Panda. All rights reserved.
+//
+
+import UIKit
+
+class MediaPickerManager: NSObject {
+
+}
