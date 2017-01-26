6e5be383dc6227a3c10df3ba4bca379c4554305e
yongrenwang <czie.alexmak@gmail.com>
2017-01-26 18:16:32 +0800

    Added and configured a button as a stored property to the PhotoList controller; also, added button as subview to viewcontroller's rootview and configured its constraints

--- FaceSnap/ViewController.swift
+++ FaceSnap/ViewController.swift
@@ -10,6 +10,20 @@ import UIKit
 
 class PhotoListController: UIViewController {
 
+    lazy var cameraButton: UIButton = {
+        let button = UIButton(type: UIButtonType.system)
+        
+
+        button.setTitle("Camera", for: UIControlState.normal)
+        button.tintColor = UIColor.white
+        button.backgroundColor = UIColor(red: 254/255.0, green: 123/255.0, blue: 135/255.0, alpha: 1.0)
+        
+        button.addTarget(self, action: #selector(PhotoListController.presentImagePickerController), for: .touchUpInside)
+
+        return button
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +34,28 @@ class PhotoListController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    
+    //MARK: - Layout
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        view.addSubview(cameraButton)
+        cameraButton.translatesAutoresizingMaskIntoConstraints = false
+        
+        
+        NSLayoutConstraint.activate([
+            cameraButton.leftAnchor.constraint(equalTo: view.leftAnchor),
+            cameraButton.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            cameraButton.rightAnchor.constraint(equalTo: view.rightAnchor),
+            cameraButton.heightAnchor.constraint(equalToConstant: 56.0)
+            ])
+    }
+    
+    //MARK: - Image Picker Controller
 
+    @objc private func presentImagePickerController(){
+        
+    }
 }
 
